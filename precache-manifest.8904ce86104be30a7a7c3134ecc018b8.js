2877d9bf0dde8bb595689ee3d79bad407436dfa4
ahtrag <garthaprasidhiyanta@gmail.com>
2019-09-20 11:35:05 +0700

    Test github.io deploy

---
+++ precache-manifest.8904ce86104be30a7a7c3134ecc018b8.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cf4232323362f9a1e0d96d235e1d19c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4646cf6812f9a582d9e1",
+    "url": "/static/js/2.65372c47.chunk.js"
+  },
+  {
+    "revision": "aa674860b1eb8dcf2f7a",
+    "url": "/static/js/main.22375444.chunk.js"
+  },
+  {
+    "revision": "4bd00109100b26673a52",
+    "url": "/static/js/runtime~main.68e2958b.js"
+  }
+]);
