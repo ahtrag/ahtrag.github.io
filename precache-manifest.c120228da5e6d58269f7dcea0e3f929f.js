883a9cbbc0f94d17a43366d494d00335803cbca2
ahtrag <garthaprasidhiyanta@gmail.com>
2021-02-05 08:34:47 +0700

    Update to porto

---
+++ precache-manifest.c120228da5e6d58269f7dcea0e3f929f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f4e50e3c4ad5be2dfee0dd143f60be3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ca70c91e1fe5934f667",
+    "url": "/static/css/main.1d96f509.chunk.css"
+  },
+  {
+    "revision": "7ec7926091c6642b0f03",
+    "url": "/static/js/2.957d352c.chunk.js"
+  },
+  {
+    "revision": "a09bcd6464a11c77a5caeae57fde5da8",
+    "url": "/static/js/2.957d352c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ca70c91e1fe5934f667",
+    "url": "/static/js/main.ed0ea8c5.chunk.js"
+  },
+  {
+    "revision": "2903eb1f4c21d3b2e2e5",
+    "url": "/static/js/runtime-main.89ba0b7d.js"
+  },
+  {
+    "revision": "089e864be75dce731b8e9804d9fbabfc",
+    "url": "/static/media/profile_photo.089e864b.jpg"
+  },
+  {
+    "revision": "d05d4a3fb1b0bb5a3343eb90806eb58b",
+    "url": "/static/media/project1.d05d4a3f.png"
+  },
+  {
+    "revision": "013530a50d6cce67cdf3f059b3b078d3",
+    "url": "/static/media/project2.013530a5.png"
+  },
+  {
+    "revision": "7222473556ba29f8bef1be7d8044d3e1",
+    "url": "/static/media/project3.72224735.png"
+  }
+]);
